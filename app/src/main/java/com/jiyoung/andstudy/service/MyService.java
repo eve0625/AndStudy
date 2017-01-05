329c9fa402d4b956a0dcf1c6f987f36a6c556691
Shin Ji Young <eve0625@naver.com>
2017-01-06 02:09:31 +0900

    change Service

--- app/src/main/java/com/jiyoung/andstudy/service/MyService.java
+++ app/src/main/java/com/jiyoung/andstudy/service/MyService.java
@@ -9,30 +9,12 @@ import android.widget.Toast;
 public class MyService extends Service {
 
     private static final String TAG = "MyService";
-    boolean isRunning;
     int mCount;
 
     @Override
     public void onCreate() {
         super.onCreate();
         Toast.makeText(this, "my service called : onCreate", Toast.LENGTH_SHORT).show();
-
-        isRunning = true;
-        mCount = 0;
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while(isRunning) {
-                    Log.i(TAG, "my service called : " + mCount);
-                    mCount++;
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
     }
 
     @Override
@@ -43,9 +25,10 @@ public class MyService extends Service {
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
+    public int onStartCommand(Intent intent, int flags, final int startId) {
         //서비스 시작시 호출됨
         Toast.makeText(this, "my service called : onStartCommand", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, String.format("my service called : onStartCommand => %d", startId));
 
         //Service 호출시 보낸 파라미터 꺼내기
         if (intent != null) {
@@ -53,6 +36,25 @@ public class MyService extends Service {
             this.mCount = count;
             Log.i(TAG, String.format("my service called : count=%d", count));
         }
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                for (int i = 0; i < 10; i++) {
+                    synchronized (this) {
+                        try {
+                            wait(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                //작업이 끝나면 스스로 종료
+                stopSelf();
+            }
+        }).start();
+
         //서비스가 메모리 부족등의 이유로 강제로 죽은 경우, 다시 띄우지 않음. (다시 띄우려면 START_STICKY)
         return Service.START_NOT_STICKY;
     }
@@ -60,8 +62,7 @@ public class MyService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
-
+        Log.i(TAG, "my service called : onDestroy");
         Toast.makeText(this, "my service called : onDestroy", Toast.LENGTH_SHORT).show();
-        isRunning = false;
     }
 }
