8de586d71908e54f300342468e6b58cef1332a87
Shin Ji Young <eve0625@naver.com>
2017-01-10 15:29:13 +0900

    GridLayout Example

---
+++ app/src/main/java/com/jiyoung/andstudy/activity/GridLayoutActivity.java
@@ -0,0 +1,16 @@
+
+package com.jiyoung.andstudy.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jiyoung.andstudy.R;
+
+public class GridLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_layout);
+    }
+}
